60244f7cddbc86c0860eaa8faa8833b14712d839
George <george.vince@hotmail.co.uk>
2022-01-06 13:32:59 +0000

    added: updated producer

--- README.md
+++ README.md
@@ -3,16 +3,24 @@
 
 ## Producer
 - Created using `python`
-- Simulates some junk data representing riders, their location and journey length.#
+- Simulates some junk data representing riders, their location and the start / end time of journeys
 - Pushes this to the kafka topic `riders`
 
 ### Running Kafka
+Note - Kafka topic is automatically created with `KAFKA_AUTO_CREATE_TOPICS_ENABLE: "true"`
 
 ```
 # Ensure `kafka` is up and running first
 
 docker-compose up - d
 ```
+
+#### Useful Kafka references:
+- https://kafka.apache.org/quickstart
+- https://docs.confluent.io/platform/current/quickstart/ce-docker-quickstart.html
+- https://rmoff.net/2018/08/02/kafka-listeners-explained/
+
+
 
 ### Running Producer
 ```
--- producer/producer.py
+++ producer/producer.py
@@ -5,31 +5,68 @@ from random import randint
 import json
 from dataclasses import dataclass, asdict
 from typing import List
+from datetime import datetime, timedelta
 
 fake = Faker("en_GB")
+
+total_riders = 0
+total_time = 0
 
 
 @dataclass
 class Rider:
     name: str
     address: str
-    ride_length: int
+    event_time: str
+    event_type: str
 
 
-def generate_riders(num: int) -> List[Rider]:
+def generate_start_time(num: int) -> List[Rider]:
     print(f"Generating {num} riders data...")
     riders = []
     for _ in range(num):
         riders.append(
-            Rider(name=fake.name(), address=fake.address(), ride_length=randint(1, 100))
+            Rider(
+                name=fake.name(),
+                address=fake.address(),
+                event_time=datetime.now().isoformat(),
+                event_type="START",
+            )
         )
     return riders
+
+
+def push_end_times(producer: KafkaProducer, riders: List[Rider]) -> None:
+
+    global total_riders
+    global total_time
+
+    for rider in riders:
+        rider.event_type = "END"
+        cur_time = datetime.fromisoformat(rider.event_time)
+        extra_minutes = randint(1, 100)
+        new_time = cur_time + timedelta(minutes=extra_minutes)
+        rider.event_time = new_time.isoformat()
+
+        total_riders += 1
+        total_time += extra_minutes
+
+    push_to_kafka(producer, riders)
 
 
 def push_to_kafka(producer: KafkaProducer, riders: List[Rider]) -> None:
     print(f"Pushing {len(riders)} to kafka")
     for rider in riders:
         producer.send("riders", value=asdict(rider))
+
+
+def report_average() -> None:
+    global total_riders
+    global total_time
+
+    print(
+        f"Total riders: {total_riders} - Total time: {total_time} - Avg time: {total_time/total_riders}"
+    )
 
 
 def produce() -> None:
@@ -39,8 +76,14 @@ def produce() -> None:
     )
 
     while True:
-        riders = generate_riders(randint(1, 10))
+        riders = generate_start_time(randint(1, 10))
+
+        print("BATCH START")
         push_to_kafka(producer, riders)
+        sleep(randint(0, 200) / 100)
+        push_end_times(producer, riders)
+        print("BATCH END")
+        report_average()
         sleep(randint(0, 200) / 100)
 
 
